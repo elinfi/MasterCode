ebfb48da7a83d77e38c42097535e87e42e9fe27c
elinfi <elin.finstad@hotmail.com>
2022-02-21 17:27:35 +0100

    Changed to pick tad inside change functions. Added option for changing tad-tad interactions.

--- clustering/class/simulate_data.py
+++ clustering/class/simulate_data.py
@@ -14,32 +14,31 @@ from data_preparation import DataPreparation
 
 
 class SimulateData():
-    def __init__(self, resolution, max_range):
+    def __init__(self, resolution, max_range, ntads):
         """
         Simulate Hi-C data.
         
         Args:
             resolution (int):
-                Resolution for cooler object
+                Resolution for cooler object.
             max_range (int):
                 Size of genomic range.
+            ntads (int):
+                Minimum number of TADs to be within genomic range.
         Attrs:
             matrix (ndarray):
                 Numpy zeroes array.
         """
-        #self.mat_region = sim.get_region(max_range)
-        self.mat_region = 'chr8:85013332-95013332'
+        self.resolution = resolution
+        self.mat_region, self.df = sim.get_region_with_ntads(max_range, ntads)
+        #self.mat_region = 'chr8:85013332-95013332'
         #self.tad_region = sim.get_tad_region(self.mat_region)
-        self.tad_region = 'chr8:86550000-87600000'
+        #self.tad_region = 'chr8:86550000-87600000'
         
-        self.mat1 = sim.matrix(self.mat_region, 'wt_001', resolution)
-        self.mat2 = sim.matrix(self.mat_region, 'wt_002', resolution)
-        
-        self.tad_i, self.tad_j = sim.get_tad_idx(self.mat_region, 
-                                                 self.tad_region,
-                                                 resolution)
+        self.mat1 = sim.matrix(self.mat_region, 'wt_001', self.resolution)
+        self.mat2 = sim.matrix(self.mat_region, 'wt_002', self.resolution)
     
-    def change_tad(self, change):
+    def change_tad(self, change, n=1, random_state=None):
         """Change TAD in mat2
         
         Args:
@@ -47,9 +46,36 @@ class SimulateData():
                 Function for how to change the TAD.
                 Example of function for doubling IF in TAD: lambda x: 2*x
         """
-        i, j = (self.tad_i, self.tad_j)
+        tad_regions = sim.df2tad(self.df, 1, random_state)
         
+        for tad_region in tad_regions:
+            i, j = sim.get_tad_idx(self.mat_region, 
+                                   tad_region,
+                                   self.resolution)
+
+            self.mat2[i:j, i:j] = change(self.mat2[i:j, i:j])
+        """
+        tad_region = tad_regions[0]
+        i, j = sim.get_tad_idx(self.mat_region, 
+                               tad_region,
+                               self.resolution)
+        """
         self.mat2[i:j, i:j] = change(self.mat2[i:j, i:j])
+            
+    def change_tad_tad(self, change, random_state=None):
+        tad_regions = sim.df2tad(self.df, 2, random_state)
+        print(tad_regions)
+        
+        i1, j1 = sim.get_tad_idx(self.mat_region, 
+                                 tad_regions[0],
+                                 self.resolution)
+        i2, j2 = sim.get_tad_idx(self.mat_region, 
+                                 tad_regions[1],
+                                 self.resolution)
+        print(i1, j1)
+        print(i2, j2)
+        self.mat2[i1:j1, i2:j2] = change(self.mat2[i1:j1, i2:j2])
+        self.mat2[i2:j2, i1:j1] = change(self.mat2[i2:j2, i1:j1])
         
     def compare(self, method):
         if method == 'ratio':
