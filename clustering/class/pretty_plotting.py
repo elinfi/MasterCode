ad2434a9082482aafc51f7c33e10f612ad906375
elinfi <elin.finstad@hotmail.com>
2022-02-21 17:26:33 +0100

    Added font_size function.

--- clustering/class/pretty_plotting.py
+++ clustering/class/pretty_plotting.py
@@ -1,4 +1,5 @@
 import simulation_functions as sim
+import matplotlib.pyplot as plt
 
 from matplotlib.ticker import EngFormatter
 
@@ -19,3 +20,12 @@ def region2extent(region):
     extent = (start, end, end, start)
     
     return extent
+
+def font_size(SMALL_SIZE=12, MEDIUM_SIZE=14, BIGGER_SIZE=16):
+    plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+    plt.rc('axes', titlesize=BIGGER_SIZE)    # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
