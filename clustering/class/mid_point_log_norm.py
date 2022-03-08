21510fb1b7468507c4847450a20c01e4ba428eb8
elinfi <elin.finstad@hotmail.com>
2022-03-08 09:58:50 +0100

    Small changes for more reliable code.

--- clustering/class/mid_point_log_norm.py
+++ clustering/class/mid_point_log_norm.py
@@ -6,7 +6,10 @@ class MidPointLogNorm(LogNorm):
     def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
         LogNorm.__init__(self,vmin=vmin, vmax=vmax, clip=clip)
         self.midpoint=midpoint
+        
+        
     def __call__(self, value, clip=None):
         result, is_scalar = self.process_value(value)
+        self.autoscale_None(result)
         x, y = [np.log(self.vmin), np.log(self.midpoint), np.log(self.vmax)], [0, 0.5, 1]
-        return np.ma.array(np.interp(np.log(value), x, y), mask=result.mask, copy=False)
+        return np.ma.array(np.interp(np.log(result), x, y), mask=result.mask, copy=False)
