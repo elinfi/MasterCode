f815bca2c463dcf51b32471cb8f8a0e3f4d0f52b
elinfi <elin.finstad@hotmail.com>
2022-04-22 15:55:29 +0200

    Added log to init

--- src/class/mid_point_log_norm.py
+++ src/class/mid_point_log_norm.py
@@ -3,13 +3,14 @@ import numpy as np
 from  matplotlib.colors import LogNorm
 
 class MidPointLogNorm(LogNorm):
-    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+    def __init__(self, vmin=None, vmax=None, midpoint=None, log=np.log2, clip=False):
         LogNorm.__init__(self,vmin=vmin, vmax=vmax, clip=clip)
         self.midpoint=midpoint
+        self.log = log
         
         
     def __call__(self, value, clip=None):
         result, is_scalar = self.process_value(value)
         self.autoscale_None(result)
-        x, y = [np.log2(self.vmin), np.log2(self.midpoint), np.log2(self.vmax)], [0, 0.5, 1]
-        return np.ma.array(np.interp(np.log2(result), x, y), mask=result.mask, copy=False)
+        x, y = [self.log(self.vmin), self.log(self.midpoint), self.log(self.vmax)], [0, 0.5, 1]
+        return np.ma.array(np.interp(self.log(result), x, y), mask=result.mask, copy=False)
